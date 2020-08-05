1cfe6586014376084ec62616dc6378ae4daadd94
wohc888 <suess98@naver.com>
2020-08-05 16:37:44 +0900

    d3
    d3,  mainly about handling events

---
+++ d3/src/index.js
@@ -0,0 +1,44 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+/*
+✅ The text of the title should change when the mouse is on top of it.
+✅ The text of the title should change when the mouse is leaves it.
+✅ When the window is resized the title should change.
+✅ On right click the title should also change.
+✅ The colors of the title should come from a color from the colors array.
+✅ DO NOT CHANGE .css, or .html files.
+✅ ALL function handlers should be INSIDE of "superEventHandler"
+*/
+
+const target = document.querySelector("h2");
+
+const superEventHandler = {
+  mouseEnter: function handleMouseEnter() {
+    target.innerText = "The mouse is here!";
+    target.style.color = colors[0];
+  },
+  mouseLeave: function handleMouseLeave() {
+    target.innerText = "The mouse is gone!";
+    target.style.color = colors[1];
+  },
+  mouseRightClick: function handleRightClick() {
+    target.innerText = "That was a right click!";
+    target.style.color = colors[2];
+  },
+  resize: function handleResize() {
+    target.innerText = "You just resized!";
+    target.style.color = colors[3];
+  }
+};
+
+function init() {
+  window.addEventListener("resize", superEventHandler.resize);
+  window.addEventListener("contextmenu", superEventHandler.mouseRightClick);
+  target.addEventListener("mouseenter", superEventHandler.mouseEnter);
+  target.addEventListener("mouseleave", superEventHandler.mouseLeave);
+}
+
+init();
